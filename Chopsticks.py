e50771fab0b586ff08bbe4343e33d5dcb37e2418
Chris Campell <ccampell.asu@gmail.com>
2017-10-03 20:51:28 -0400

    Fixed the bug, it works now except for the methods that are not implemented yet.

--- Chopsticks.py
+++ Chopsticks.py
@@ -18,15 +18,8 @@ def main(num_hands, num_fingers):
     """
     # Initialize the game by creating an instance of the ChopsticksGame inherited from the Game class:
     chopsticks = ChopsticksGame(num_hands, num_fingers)
-    # Display the initial gamestate:
-    # chopsticks.display(chopsticks.initial)
-    # Create a random player:
-    random_agent = random_player(game=chopsticks, state=chopsticks.initial)
-    # Create a human player:
-    human_agent = query_player(game=chopsticks, state=chopsticks.initial)
-    # Play a game of Chopsticks:
-    chopsticks.play_game(random_agent, human_agent)
+    # Play a game of Chopsticks against a human:
+    chopsticks.play_game(random_player, query_player)
 
 if __name__ == '__main__':
-    # TODO: Perform any declarations of project relative file paths, etc..
     main(num_hands=2, num_fingers=5)
