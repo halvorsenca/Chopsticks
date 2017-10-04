1cf63d2d52dc5e70e30a630de5f9eeec03e32b2b
Chris Campell <ccampell.asu@gmail.com>
2017-10-03 20:37:19 -0400

    Added setup for skeleton game environment.

--- Chopsticks.py
+++ Chopsticks.py
@@ -3,6 +3,9 @@ Chopsticks.py
 Program Three Implementation.
 """
 from ChopsticksGame import ChopsticksGame
+from aima.games import random_player
+from aima.games import query_player
+from aima.games import alphabeta_player, alphabeta_cutoff_search
 
 __author__ = "Chris Campell, Cydney Caldwell, Chad Halvorsen"
 __version__ = "10/2/2017"
@@ -16,8 +19,13 @@ def main(num_hands, num_fingers):
     # Initialize the game by creating an instance of the ChopsticksGame inherited from the Game class:
     chopsticks = ChopsticksGame(num_hands, num_fingers)
     # Display the initial gamestate:
-    chopsticks.display(chopsticks.initial)
-
+    # chopsticks.display(chopsticks.initial)
+    # Create a random player:
+    random_agent = random_player(game=chopsticks, state=chopsticks.initial)
+    # Create a human player:
+    human_agent = query_player(game=chopsticks, state=chopsticks.initial)
+    # Play a game of Chopsticks:
+    chopsticks.play_game(random_agent, human_agent)
 
 if __name__ == '__main__':
     # TODO: Perform any declarations of project relative file paths, etc..
