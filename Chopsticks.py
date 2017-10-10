9ab6b49ecf58ac1ea32ab644265ab40faf3ce8da
Chris Campell <ccampell.asu@gmail.com>
2017-10-10 13:30:59 -0400

    Added code for minimax player which currently does not work due to infinite loops.

--- Chopsticks.py
+++ Chopsticks.py
@@ -3,13 +3,15 @@ Chopsticks.py
 Program Three Implementation.
 """
 from ChopsticksGame import ChopsticksGame
-from aima.games import random_player
-from aima.games import query_player
+from aima.games import random_player, query_player
 from aima.games import alphabeta_player, alphabeta_cutoff_search
+from aima.games import minimax_decision
 
 __author__ = "Chris Campell, Cydney Caldwell, Chad Halvorsen"
 __version__ = "10/2/2017"
 
+def minimax_player(game, state):
+    return minimax_decision(state=state, game=game)
 
 def main(num_hands, num_fingers):
     """
@@ -21,7 +23,7 @@ def main(num_hands, num_fingers):
     # Initialize the game by creating an instance of the ChopsticksGame inherited from the Game class:
     chopsticks = ChopsticksGame(num_hands, num_fingers)
     # Play a game of Chopsticks against a human (cpu goes first):
-    chopsticks.play_game(random_player, query_player)
+    chopsticks.play_game(minimax_player, query_player)
 
 if __name__ == '__main__':
     main(num_hands=2, num_fingers=5)
